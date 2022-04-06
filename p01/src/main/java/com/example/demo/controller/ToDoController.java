83f6681d24a22b338c7e98d02d29a94b667f4f90
uematsu-ctx <hornet250tu3@docomo.ne.jp>
2022-04-06 13:42:01 +0900

    コメントアウトの追加

--- p01/src/main/java/com/example/demo/controller/ToDoController.java
+++ p01/src/main/java/com/example/demo/controller/ToDoController.java
@@ -131,7 +131,9 @@ public class ToDoController {
 		//日付のインスタンスの生成
 		Date date = new Date();
 
+		//登録日をデータベースへ入力
 		td.setRegistration_date(date);
+
 		//完了していたら日付を、完了していなかったら未と表示。
 		if (form.getCheck()) {
 			td.setFinished_date(date);
@@ -152,7 +154,7 @@ public class ToDoController {
 			model.addAttribute("result", "登録失敗(トランザクション)");
 			e.printStackTrace();
 		}
-		
+
 		//list.htmlに画面遷移
 		return getToDoList(model);
 	}
